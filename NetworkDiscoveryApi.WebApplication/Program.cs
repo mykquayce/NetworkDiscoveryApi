d9f6399e027741d358898497128ef5dfafce341b
Bob H <mail@bob.codes>
2021-05-07 12:07:03 +0100

    switch to using ssh keys via docker secrets

--- NetworkDiscoveryApi.WebApplication/Program.cs
+++ NetworkDiscoveryApi.WebApplication/Program.cs
@@ -17,7 +17,6 @@ namespace NetworkDiscoveryApi.WebApplication
 				.ConfigureAppConfiguration((context, configBuilder) =>
 				{
 					configBuilder
-						.AddDockerSecrets(optional: true, reloadOnChange: true)
 						.AddUserSecrets(typeof(Program).Assembly, optional: true, reloadOnChange: true);
 				});
 
