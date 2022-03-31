d40ee961e4ab9131ba2dd4a7e15f1528c15dc73e
bob h <mail@bob.codes>
2022-03-31 02:22:34 +0100

    fixing integration test
    401 error

---
+++ NetworkDiscoveryApi.WebApplication.Tests/Fixtures/IdentityClientFixture.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace NetworkDiscoveryApi.WebApplication.Tests.Fixtures;
+
+public sealed class IdentityClientFixture : IDisposable
+{
+	private static readonly Uri _authority = new("https://identityserver");
+	private const string _cliendId = "elgatoapi";
+	private const string _clientSecret = "8556e52c6ab90d042bb83b3f0c8894498beeb65cf908f519a2152aceb131d3ee";
+	private const string _scope = "networkdiscovery";
+
+	private readonly HttpClient _httpClient;
+	private readonly IMemoryCache _memoryCache;
+
+	public IdentityClientFixture()
+	{
+		var httpClientHandler = new HttpClientHandler { AllowAutoRedirect = false, };
+		_httpClient = new HttpClient(httpClientHandler) { BaseAddress = _authority, };
+		var config = new Helpers.Identity.Config(_authority, _cliendId, _clientSecret, _scope);
+		_memoryCache = new MemoryCache(new MemoryCacheOptions());
+
+		IdentityClient = new Helpers.Identity.Clients.Concrete.IdentityClient(config, _httpClient, _memoryCache);
+	}
+
+	public Helpers.Identity.Clients.IIdentityClient IdentityClient { get; }
+
+	#region Dispose
+	public void Dispose()
+	{
+		_httpClient.Dispose();
+		_memoryCache.Dispose();
+	}
+	#endregion Dispose
+}
--- NetworkDiscoveryApi.WebApplication.Tests/IntegrationTests.cs
+++ NetworkDiscoveryApi.WebApplication.Tests/IntegrationTests.cs
@@ -1,23 +1,30 @@
+using IdentityModel.Client;
 using Xunit;
 
 namespace NetworkDiscoveryApi.WebApplication.Tests;
 
-public class IntegrationTests : IClassFixture<Fixtures.WebHostFixture>
+public class IntegrationTests : IClassFixture<Fixtures.WebHostFixture>, IClassFixture<Fixtures.IdentityClientFixture>
 {
 	private readonly HttpClient _httpClient;
+	private readonly Helpers.Identity.Clients.IIdentityClient _identityClient;
 
-	public IntegrationTests(Fixtures.WebHostFixture fixture)
+	public IntegrationTests(Fixtures.WebHostFixture webHostFixture, Fixtures.IdentityClientFixture identityClientFixture)
 	{
-		_httpClient = fixture.HttpClient;
+		_httpClient = webHostFixture.HttpClient;
+		_identityClient = identityClientFixture.IdentityClient;
 	}
 
 	[Fact]
 	public async Task GetDhcpLicenses()
 	{
+		var accessToken = await _identityClient.GetAccessTokenAsync();
+
+		_httpClient.SetBearerToken(accessToken);
+
 		var response = await _httpClient.GetStringAsync("/api/router");
 
 		Assert.NotNull(response);
 		Assert.NotEmpty(response);
-		Assert.Equal('[', response[0]);
+		Assert.StartsWith("[", response);
 	}
 }
