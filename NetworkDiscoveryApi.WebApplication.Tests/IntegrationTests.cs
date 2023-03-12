c4663a941868a9b269cbaaab73035de508722e3b
bob hall <myk_quayce@hotmail.com>
2023-03-12 23:13:09 +0000

    removing aliases

--- NetworkDiscoveryApi.WebApplication.Tests/IntegrationTests.cs
+++ NetworkDiscoveryApi.WebApplication.Tests/IntegrationTests.cs
@@ -20,21 +20,6 @@ public class IntegrationTests : IClassFixture<Fixtures.WebHostFixture>, IClassFi
 	public string AccessToken => _accessToken ??= _identityClient.GetAccessTokenAsync().GetAwaiter().GetResult();
 
 	[Theory]
-	[InlineData("vr front")]
-	[InlineData("VR FRONT")]
-	public async Task GetDhcpLicenseForAlias(string alias)
-	{
-		_httpClient.SetBearerToken(AccessToken);
-
-		var response = await _httpClient.GetStringAsync("api/router/" + HttpUtility.UrlPathEncode(alias));
-
-		Assert.NotNull(response);
-		Assert.NotEmpty(response);
-		Assert.StartsWith("{", response);
-		Assert.NotEqual("{}", response);
-	}
-
-	[Theory]
 	[InlineData("3c:6a:9d:14:d7:65")]
 	[InlineData("3C:6A:9D:14:D7:65")]
 	[InlineData("3c6a9d14d765")]
